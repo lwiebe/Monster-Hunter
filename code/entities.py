1aef25cdf6b1c8cb270ee942abdc2463f3480333
Lian Wiebe <72026264+lwiebe@users.noreply.github.com>
2025-03-09 18:17:49 +0100

    layers & shadow

--- code/entities.py
+++ code/entities.py
@@ -3,6 +3,7 @@ from settings import *
 class Entity(pygame.sprite.Sprite):
     def __init__(self, pos, frames, groups, facing_direction):
         super().__init__(groups)
+        self.z = WORLD_LAYERS['main']
         
         # graphics
         self.frame_index, self.frames = 0, frames
@@ -15,6 +16,8 @@ class Entity(pygame.sprite.Sprite):
         # sprite setup
         self.image = self.frames[self.get_state()][self.frame_index]
         self.rect = self.image.get_frect(center = pos)
+        
+        self.y_sort = self.rect.centery
     
     def animate(self,dt):
         self.frame_index += ANIMATION_SPEED * dt
@@ -54,6 +57,7 @@ class Player(Entity):
         self.rect.center += self.direction * self.speed * dt
     
     def update(self, dt):
+        self.y_sort = self.rect.centery
         self.input()
         self.move(dt)
         self.animate(dt)
--- code/main.py
+++ code/main.py
@@ -2,7 +2,7 @@ from settings import *
 from pytmx.util_pygame import load_pygame
 from os.path import join
 
-from sprites import Sprite, AnimatedSprite
+from sprites import Sprite, AnimatedSprite, MonsterPatchSprite
 from entities import Player, Character
 from groups import AllSprites
 
@@ -36,11 +36,30 @@ class Game:
         # terrain
         for layer in ['Terrain', 'Terrain Top']:
             for x, y, surf in tmx_map.get_layer_by_name(layer).tiles():
-                Sprite((x * TILE_SIZE, y * TILE_SIZE), surf, self.all_sprites)
+                Sprite((x * TILE_SIZE, y * TILE_SIZE), surf, self.all_sprites, WORLD_LAYERS['bg'])
            
+        # water
+        for obj in tmx_map.get_layer_by_name('Water'):
+            for x in range(int(obj.x), int(obj.x + obj.width), TILE_SIZE):
+                for y in range(int(obj.y), int(obj.y + obj.height), TILE_SIZE):
+                    AnimatedSprite((x,y), self.overworld_frames['water'], self.all_sprites, WORLD_LAYERS['water'])
+        
+        # coast
+        for obj in tmx_map.get_layer_by_name('Coast'):
+            terrain = obj.properties['terrain']
+            side = obj.properties['side']
+            AnimatedSprite((obj.x, obj.y), self.overworld_frames['coast'][terrain][side], self.all_sprites, WORLD_LAYERS['bg'])
+            
         # objects 
         for obj in tmx_map.get_layer_by_name('Objects'):
-            Sprite((obj.x, obj.y), obj.image, self.all_sprites)
+            if obj.name == 'top':
+                Sprite((obj.x, obj.y), obj.image, self.all_sprites, WORLD_LAYERS['top'])
+            else:
+                Sprite((obj.x, obj.y), obj.image, self.all_sprites)
+
+        # grass patches
+        for obj in tmx_map.get_layer_by_name('Monsters'):
+            MonsterPatchSprite((obj.x, obj.y), obj.image, self.all_sprites, obj.properties['biome'])
             
         # entities
         for obj in tmx_map.get_layer_by_name('Entities'):
@@ -57,22 +76,11 @@ class Game:
                     frames = self.overworld_frames['characters'][obj.properties['graphic']], 
                     groups = self.all_sprites,
                     facing_direction = obj.properties['direction'])
-                
-        # water
-        for obj in tmx_map.get_layer_by_name('Water'):
-            for x in range(int(obj.x), int(obj.x + obj.width), TILE_SIZE):
-                for y in range(int(obj.y), int(obj.y + obj.height), TILE_SIZE):
-                    AnimatedSprite((x,y), self.overworld_frames['water'], self.all_sprites)
-        
-        # coast
-        for obj in tmx_map.get_layer_by_name('Coast'):
-            terrain = obj.properties['terrain']
-            side = obj.properties['side']
-            AnimatedSprite((obj.x, obj.y), self.overworld_frames['coast'][terrain][side], self.all_sprites)
-        
+                     
     def run(self):
         while True:
             dt = self.clock.tick() / 1000
+            self.display_surface.fill('black')
             # event loop
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -81,7 +89,6 @@ class Game:
                     
             # game logic
             self.all_sprites.update(dt)
-            self.display_surface.fill('black')
             self.all_sprites.draw(self.player.rect.center)
             pygame.display.update()
             

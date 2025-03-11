6e1a3f52016708fee74a84da16416a44deed5b60
Lian Wiebe <72026264+lwiebe@users.noreply.github.com>
2025-03-11 18:18:45 +0100

    transitions and maps

--- code/support.py
+++ code/support.py
@@ -79,7 +79,12 @@ def coast_importer(cols, rows, *path):
 			new_dict[terrain][key] = [frame_dict[(pos[0] + index * 3, pos[1] + row)] for row in range(0,rows, 3)]
 	return new_dict
 
-
+def tmx_importer(*path):
+    tmx_dict = {}
+    for folder_path, sub_folders, file_names in walk(join(*path)):
+        for file in file_names:
+            tmx_dict[file.split('.')[0]] = load_pygame(join(folder_path, file))
+    return tmx_dict
 
 # game functions
 def check_connections(radius, entity, target, tolerance = 30):

5d5e364d5e64f566e29a16236b0b164707bddc6d
Lian Wiebe <72026264+lwiebe@users.noreply.github.com>
2025-03-19 11:20:29 +0100

    monster index

--- code/main.py
+++ code/main.py
@@ -64,7 +64,9 @@ class Game:
         }
         
         self.monster_frames = {
-            'icons': import_folder_dict('graphics', 'icons')
+            'icons': import_folder_dict('graphics', 'icons'),
+            'monsters': monster_importer(4,2,'graphics', 'monsters'),
+            'ui': import_folder_dict('graphics', 'ui')
         }
         
         self.fonts = {

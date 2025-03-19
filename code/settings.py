e9672d0ebb13b86cca7bf071c1b4fc866c4adb95
Lian Wiebe <72026264+lwiebe@users.noreply.github.com>
2025-03-19 11:05:58 +0100

    full settings

--- code/settings.py
+++ code/settings.py
@@ -1,9 +1,9 @@
 import pygame
-from pygame.math import Vector2 as vector
+from pygame.math import Vector2 as vector 
 from sys import exit
 
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
-TILE_SIZE = 64
+TILE_SIZE = 64 
 ANIMATION_SPEED = 6
 BATTLE_OUTLINE_WIDTH = 4
 
@@ -20,7 +20,9 @@ COLORS = {
 	'plant': '#64a990', 
 	'black': '#000000', 
 	'red': '#f03131',
-	'blue': '#66d7ee'
+	'blue': '#66d7ee',
+	'normal': '#ffffff',
+	'dark white': '#f0f0f0'
 }
 
 WORLD_LAYERS = {
